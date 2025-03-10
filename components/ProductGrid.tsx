c3454bf1b7d2f0ce0df6d83f91d0e7a9d5ce94b3
Baburao Adkane <b.g.adkane27@gmail.com>
2025-03-10 08:20:13 +0530

    loading added

--- components/ProductGrid.tsx
+++ components/ProductGrid.tsx
@@ -7,6 +7,7 @@ import { client } from "@/sanity/lib/client";
 import { Product } from "@/sanity.types";
 import ProductCard from "./ProductCard";
 import NoProduct from "./NoProduct";
+import { LoaderIcon } from "lucide-react";
 
 const ProductGrid = () => {
   const [selectedTab, setSelectedTab] = useState(productData[0]?.title || "");
@@ -35,8 +36,11 @@ const ProductGrid = () => {
     <div className="mt-10 flex flex-col items-center">
       <HomeTabbar selectedTab={selectedTab} onTabSelect={setSelectedTab} />
       {loading ? (
-        <div>
-          <span>Product is loading...</span>
+        <div className="mt-5 flex flex-col items-center justify-center p-6 min-h-60 space-y-4 text-center rounded-md bg-gray-100 w-full">
+          <div className="flex items-center space-x-2 text-blue-600">
+            <LoaderIcon className="animate-spin"/>
+            <span className="text-lg font-semibold">Loading...</span>
+          </div>
         </div>
       ) : products.length ? (
         <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 w-full">
