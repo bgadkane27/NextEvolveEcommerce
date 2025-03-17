9e9633880dd9fb6a2541bae324f7a1f740028aae
Baburao Adkane <b.g.adkane27@gmail.com>
2025-03-18 00:39:06 +0530

    product page

--- app/(client)/product/[slug]/page.tsx
+++ app/(client)/product/[slug]/page.tsx
@@ -1,12 +1,21 @@
 import Container from '@/components/Container'
+import ImageView from '@/components/ImageView';
+import { getProductBySlug } from '@/sanity/helpers/Queries';
+import { notFound } from 'next/navigation';
 import React from 'react'
 
 const SingleProductPage = async ({params}:{params:Promise<{slug:string}>}) => {
     const {slug} = await params;
-    console.log(slug);
+    const product = await getProductBySlug(slug);
+    if(!product){
+        return notFound();
+    }
     return (
-        <Container>
-            SingleProductPage
+        <Container className='py-10 flex flex-col md:flex-row gap-10'>
+                {product?.images && <ImageView images={product?.images}/>}            
+            <div className='w-full md:w-1/2 flex flex-col gap-2'>
+                <div>{product?.name}</div>
+            </div>
         </Container>
     )
 }
