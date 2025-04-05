33c82d496f7c8e93392eb56ec581efd5bc3c005a
Baburao Adkane <b.g.adkane27@gmail.com>
2025-04-05 23:23:02 +0530

    cart logic

--- app/(client)/cart/page.tsx
+++ app/(client)/cart/page.tsx
@@ -5,13 +5,14 @@ import Loading from '@/components/Loading';
 import NoAccessToCart from '@/components/NoAccessToCart';
 import useCartStore from '@/store';
 import { useAuth, useUser } from '@clerk/nextjs';
+import { ShoppingBag } from 'lucide-react';
 import React, { useEffect, useState } from 'react'
 
 const CartPage = () => {
   const [isClient, setIsClient] = useState(true);
   const { getGroupedItems, getItemCount, getTotalPrice, getSubTotalPrice, deleteCartItem, clearCart } = useCartStore();
-  const {isSignedIn} = useAuth();
-  const {user} = useUser();
+  const { isSignedIn } = useAuth();
+  const { user } = useUser();
   useEffect(() => {
     setIsClient(true);
   }, [])
@@ -20,10 +21,23 @@ const CartPage = () => {
     return <Loading />
   }
 
+  const cartProducts = getGroupedItems();
+  console.log(cartProducts);
+
   return (
     <div>
       {
-        isSignedIn ? '' : <NoAccessToCart />
+        isSignedIn
+          ? <Container>
+           <div className='p-4'>
+            <h1 className='text-2xl flex items-center gap-2 mb-2'><ShoppingBag />Shopping Cart</h1>
+            <hr />
+           </div>
+           {
+            cartProducts?.length > 0 ? '' : ''
+           }
+          </Container>
+          : <NoAccessToCart />
       }
     </div>
   )
--- components/NoAccessToCart.tsx
+++ components/NoAccessToCart.tsx
@@ -10,7 +10,7 @@ const NoAccessToCart = () => {
     return (
         <Container>
             <div className='flex flex-col items-center justify-center py-12 md:py-32 p-4'>
-                <Card className='w-full max-w-md'>
+                <Card className='w-full max-w-md bg-gradient-to-t from-pink-300 to-blue-300'>
                     <CardHeader className='space-y-0.5 text-center'>
                         <h1 className='text-2xl font-bold tracking-wide uppercase'>Evolve</h1>
                         <h2 className='text-xl font-semibold tracking-wide'>Welcome Back!</h2>
@@ -18,15 +18,15 @@ const NoAccessToCart = () => {
                     <CardContent className='space-y-4'>
                         <p className='text-center'>Log in to view your cart items and checkout. Don&apos;t miss out on your favorite products !</p>
                         <SignInButton mode='modal'>
-                            <Button color='primary' className='w-full rounded-full'>
+                            <Button color='primary' className='w-full rounded-full hover:cursor-pointer'>
                                 Sign in
                             </Button>
                         </SignInButton>
                     </CardContent>
                     <CardFooter className='flex flex-col items-center justify-center space-y-0.5'>
-                        <p className='text-muted-foreground'>Don&apos;t have an account?</p>
+                        <p className='text-gray-800'>Don&apos;t have an account?</p>
                         <SignUpButton mode='modal'>
-                            <Button color="primary" variant='bordered' className='w-full border rounded-full'>
+                            <Button color="secondary" className='w-full border border-blue-500 rounded-full hover:cursor-pointer'>
                                 Sign up
                             </Button>
                         </SignUpButton>
